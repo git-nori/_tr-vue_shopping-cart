1e206d0eebea74d8fe87c86178f79bb57763acb1
HironoriSato <maza.hs5360@gmail.com>
2020-02-28 22:49:14 +0900

    [add]Productsモデルに対してのvuexを実装

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,23 +1,31 @@
 <template>
-  <b-list-group >
+  <b-list-group>
     <div v-for="(product, i) in products" :key="i">
-      <b-list-group-item>{{ product.title }} - {{ product.price }}</b-list-group-item>
+      <b-list-group-item class="d-flex justify-content-between align-items-center">
+        {{ product.title }} - {{ product.price }}
+          <b-button variant="primary" size="sm" @click="addProductToCart(product)">Add to cart</b-button>
+      </b-list-group-item>
     </div>
   </b-list-group>
 </template>
 
 <script>
 import shop from "@/api/shop.js";
+import { mapActions, mapState } from "vuex";
 
 export default {
   name: "ProductList",
   data() {
-    return {
-      products: []
-    };
+    return {};
+  },
+  computed: {
+    ...mapState(["products"])
   },
   created() {
-    shop.getProducts(products => (this.products = products));
-  }
+    this.$store.dispatch("getAllProducts");
+  },
+  methods: {
+    ...mapActions(['addProductToCart'])
+  },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,32 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import shop from '@/api/shop.js'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    products: [],
+    items: [],
+  },
+  mutations: {
+    setProducts(state, products){
+      state.products = products
+    },
+    pushProductToCart(state, product){
+      state.items.push({
+        id: product.id,
+        quantity: 1
+      })
+    }
+  },
+  actions: {
+    getAllProducts({commit}){
+      shop.getProducts(products => { commit('setProducts', products) })
+    },
+    addProductToCart({commit}, product){
+      commit('pushProductToCart', product)
+    }
+  },
   modules: {}
 });
