ece6664ffffdf21eda2c9b3e323c59c79bf12b3a
HironoriSato <maza.hs5360@gmail.com>
2020-02-28 22:39:32 +0900

    [add]ProductList compoを作成

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <b-container>
+      <h1 class="mt-3">Shopping Cart Example</h1>
+      <hr />
+
+      <h2>Products</h2>
+      <b-row>
+        <b-col>
+          <ProductList />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
+<script>
+import ProductList from "@/components/ProductList.vue";
+
+export default {
+  name: "app",
+  components: { ProductList }
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-list-group >
+    <div v-for="(product, i) in products" :key="i">
+      <b-list-group-item>{{ product.title }} - {{ product.price }}</b-list-group-item>
+    </div>
+  </b-list-group>
+</template>
+
+<script>
+import shop from "@/api/shop.js";
+
+export default {
+  name: "ProductList",
+  data() {
+    return {
+      products: []
+    };
+  },
+  created() {
+    shop.getProducts(products => (this.products = products));
+  }
+};
+</script>
